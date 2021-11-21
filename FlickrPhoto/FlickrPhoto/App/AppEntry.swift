4e03dbc33c7abe12da0132d2f36d5ee3aca8bba3
bob <ahmedelsman0@gmail.com>
2021-11-21 15:14:08 +0200

    add app entry and use it in scene

---
+++ FlickrPhoto/FlickrPhoto/App/AppEntry.swift
@@ -0,0 +1,16 @@
+//
+//  AppEntry.swift
+//  FlickrPhoto
+//
+//  Created by Ahmed Elsman on 21/11/2021.
+//
+
+import UIKit
+
+struct AppEntry {
+    
+    func initFirstScene(window: UIWindow) {
+        window.rootViewController = UINavigationController(rootViewController: PhotosListBuilder.viewController())
+        window.makeKeyAndVisible()
+    }
+}
