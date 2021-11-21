6508ec61cf28f65b11fd6f82e6c80c4899ff2832
bob <ahmedelsman0@gmail.com>
2021-11-21 14:59:05 +0200

    adding photo list view controller and open it form app scene

---
+++ FlickrPhoto/FlickrPhoto/Scene/PhotosList/PhotosListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PhotosListViewController.swift
+//  FlickrPhoto
+//
+//  Created by Ahmed Elsman on 21/11/2021.
+//
+
+import UIKit
+
+class PhotosListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
+    }
+    
+}
