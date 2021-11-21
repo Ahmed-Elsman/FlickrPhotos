7e4eb9a097242425e13ac44cc7b2e67fba25f3b2
bob <ahmedelsman0@gmail.com>
2021-11-21 15:00:02 +0200

    adding missed files (app delegate & scene delegate) missed from previous commit

---
+++ FlickrPhoto/FlickrPhoto/App/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  FlickrPhoto
+//
+//  Created by Ahmed Elsman on 21/11/2021.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
