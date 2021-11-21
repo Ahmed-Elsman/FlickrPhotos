9243ab98ff5f15ba0a03fe56ee4113fb9f886824
bob <ahmedelsman0@gmail.com>
2021-11-21 15:05:56 +0200

    converting photo list view controller to view & builder then use it in app scene as initial view controller

--- FlickrPhoto/FlickrPhoto/App/SceneDelegate.swift
+++ FlickrPhoto/FlickrPhoto/App/SceneDelegate.swift
@@ -8,16 +8,16 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
-                window = UIWindow(windowScene: windowScene)
-                window?.rootViewController = PhotosListViewController()
-                window?.makeKeyAndVisible()
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = PhotosListBuilder.viewController()
+        window?.makeKeyAndVisible()
     }
 }
 
---
+++ FlickrPhoto/FlickrPhoto/Scene/PhotosList/PhotosListBuilder.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosListBuilder.swift
+//  FlickrPhoto
+//
+//  Created by Ahmed Elsman on 21/11/2021.
+//
+
+import Foundation
+
+struct PhotosListBuilder {
+    
+    static func viewController() -> PhotosListViewController {
+        let viewController: PhotosListViewController = PhotosListViewController()
+        return viewController
+    }
+}
