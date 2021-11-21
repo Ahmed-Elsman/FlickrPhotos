6ad3cc37f94e68e656c26af89f34c77507bd7e52
bob <ahmedelsman0@gmail.com>
2021-11-21 22:52:59 +0200

    adding Flicker error in places where errors may be returned create implement searching protocol for view model by getting data from it's repository and sink on it

--- FlickrPhoto/FlickrPhoto/Scene/PhotosList/PhotosListViewModel.swift
+++ FlickrPhoto/FlickrPhoto/Scene/PhotosList/PhotosListViewModel.swift
@@ -10,7 +10,7 @@ import Combine
 
 protocol PhotosListViewModelInput: AnyObject {
     func search(for text: String)
-    var itemsForCollection: CurrentValueSubject<[ItemCollectionViewCellType], Never> { get set }
+    var itemsForCollection: CurrentValueSubject<[ItemCollectionViewCellType], FlickrPhotoError> { get set }
 }
 
 
@@ -21,16 +21,18 @@ enum State {
 final class PhotosListViewModel {
     
     private weak var output: BaseViewModelOutput?
+    let photosRepository: WebPhotosRepository
     
     fileprivate var page: Int = 1
     fileprivate var canLoadMore = true
     
-    var itemsForCollection = CurrentValueSubject<[ItemCollectionViewCellType], Never>([])
+    var itemsForCollection = CurrentValueSubject<[ItemCollectionViewCellType], FlickrPhotoError>([])
     var state: CurrentValueSubject<State, Never> = CurrentValueSubject<State, Never>(.searchResult(term: ""))
     var cancelableSet: Set<AnyCancellable> =  Set<AnyCancellable>()
     
-    init(output: BaseViewModelOutput) {
+    init(output: BaseViewModelOutput, photosRepository: WebPhotosRepository = WebPhotosRepository()) {
         self.output = output
+        self.photosRepository = photosRepository
     }
 }
 
@@ -55,6 +57,46 @@ extension PhotosListViewModel {
     private func getData(for query: String) {
         output?.showLoading()
         canLoadMore = false
+        
+        try? photosRepository.photos(for: query, page: page).sink(receiveCompletion: { [unowned self] completion in
+            self.output?.hideLoading()
+            switch completion {
+            case .finished:
+                break
+            case .failure(let error):
+                self.itemsForCollection.send(completion: .failure(error))
+            }
+        }, receiveValue: { [unowned self] searchResult in
+            self.output?.hideLoading()
+            guard let photos = searchResult.photos, photos.currentPage < photos.totalPages  else {
+                self.handleNoPhotos()
+                return
+            }
+            self.handleNewPhotos(photos: photos)
+            self.canLoadMore = true
 
+        }).store(in: &cancelableSet)
+    }
+    
+    private func handleNewPhotos(photos: Photos) {
+        let newItems: [ItemCollectionViewCellType] = createItemsForCollection(photosArray: photos.photos)
+        itemsForCollection.value.append(contentsOf: newItems)
+        if itemsForCollection.value.isEmpty {
+            itemsForCollection.send(completion: .failure(FlickrPhotoError.noResults))
+        } else {
+            itemsForCollection.send(newItems)
+        }
+    }
+    
+    private func handleNoPhotos() {
+        if  itemsForCollection.value.isEmpty {
+            itemsForCollection.send(completion: .failure(FlickrPhotoError.noResults))
+        }
+    }
+    
+    private func createItemsForCollection(photosArray: [Photo]) -> [ItemCollectionViewCellType] {
+        return photosArray.map { photo -> ItemCollectionViewCellType  in
+                .photo(photo: photo)
+        }
     }
 }
