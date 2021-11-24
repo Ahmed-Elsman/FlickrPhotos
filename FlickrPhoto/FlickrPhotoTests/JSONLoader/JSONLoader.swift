cd70083aeb3c238e16b6f24bdb4a5c8def379212
bob <ahmedelsman0@gmail.com>
2021-11-24 14:50:28 +0200

    json loader

---
+++ FlickrPhoto/FlickrPhotoTests/JSONLoader/JSONLoader.swift
@@ -0,0 +1,25 @@
+//
+//  JsonLoader.swift
+//  FlickrPhotoTests
+//
+//  Created by Ahmed Elsman on 24/11/2021.
+//
+
+import Foundation
+
+import Foundation
+
+final class JSONLoader {
+
+     func loadJsonData(file: String) -> Data? {
+
+        if let jsonFilePath = Bundle(for: type(of: self)).path(forResource: file, ofType: "json") {
+            let jsonFileURL = URL(fileURLWithPath: jsonFilePath)
+
+            if let jsonData = try? Data(contentsOf: jsonFileURL) {
+                return jsonData
+            }
+        }
+        return nil
+    }
+}
