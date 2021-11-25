0730fe77ba97ae8e24dc0052f037968b507db323
bob <ahmedelsman0@gmail.com>
2021-11-25 08:30:26 +0200

    separation

--- FlickrPhoto/FlickrPhoto/NetworkLayer/SearchHistoryRepo/SearchHistoryRepository.swift
+++ FlickrPhoto/FlickrPhoto/NetworkLayer/SearchHistoryRepo/SearchHistoryRepository.swift
@@ -13,39 +13,3 @@ protocol SearchHistoryRepository {
     func saveSearchKeyword(searchKeyword: String) -> [String]
     func clearSearchHistory() -> [String]
 }
-
-final class UserDetaultSearchHistoryRepository: SearchHistoryRepository {
-    
-    func getSearchHistory() -> [String] {
-        if let history = UserDefaults.standard.array(forKey: UserDefaultsKey.searchHistoryOfPhotos.rawValue) as? [String] {
-            return history
-        }
-        return []
-        
-    }
-    
-    @discardableResult
-    func saveSearchKeyword(searchKeyword: String) -> [String] {
-        let history = getSearchHistory()
-        guard !searchKeyword.isEmpty else {
-            return history
-        }
-        
-        let lowercaseKeyword = searchKeyword.lowercased()
-        var result = history.filter { keyword -> Bool in
-            keyword.lowercased() != lowercaseKeyword
-        }
-        result.append(searchKeyword)
-        UserDefaults.standard.set(result, forKey: UserDefaultsKey.searchHistoryOfPhotos.rawValue)
-        return history
-    }
-    
-    func clearSearchHistory() -> [String] {
-        UserDefaults.standard.removeObject(forKey: UserDefaultsKey.searchHistoryOfPhotos.rawValue)
-        return []
-    }
-}
-
-enum UserDefaultsKey: String {
-    case searchHistoryOfPhotos
-}
