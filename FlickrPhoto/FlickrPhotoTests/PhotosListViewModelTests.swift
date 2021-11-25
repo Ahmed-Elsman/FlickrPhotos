a7420aba4cf0ec44a131a741a54c5d0099672124
bob <ahmedelsman0@gmail.com>
2021-11-25 14:13:01 +0200

    auto correct spaces

--- FlickrPhoto/FlickrPhotoTests/PhotosListViewModelTests.swift
+++ FlickrPhoto/FlickrPhotoTests/PhotosListViewModelTests.swift
@@ -10,19 +10,19 @@ import Combine
 @testable import FlickrPhoto
 
 class PhotosListViewModelTests: XCTestCase {
-    
+
     var webPhotosRepository: WebPhotosRepository!
     var photosListViewModel: PhotosListViewModel!
     var subscriptions = Set<AnyCancellable>()
-    
+
     override func setUp() {
         subscriptions = []
     }
-    
+
     func test_SearchFileSuccess() {
         webPhotosRepository = WebPhotosRepository(provider: MockAPIProvider.createMockAPIProvider(fromJsonFile: "photosData", statusCode: 200, error: nil))
-        photosListViewModel = PhotosListViewModel(output: PhotosListViewController(),photosRepository: webPhotosRepository)
-        
+        photosListViewModel = PhotosListViewModel(output: PhotosListViewController(), photosRepository: webPhotosRepository)
+
         let promise = XCTestExpectation(description: "Fetching Photos From File Success")
         photosListViewModel.search(for: "")
         photosListViewModel.itemsForCollection.dropFirst()
@@ -30,15 +30,14 @@ class PhotosListViewModelTests: XCTestCase {
                 XCTAssertGreaterThan(response.count, 0)
                 promise.fulfill()
             }.store(in: &subscriptions)
-        
+
         wait(for: [promise], timeout: 3)
     }
-    
-    
+
     func test_SearchFileFail() {
         webPhotosRepository = WebPhotosRepository(provider: MockAPIProvider.createMockAPIProvider(fromJsonFile: "noData", statusCode: 200, error: nil))
-        photosListViewModel = PhotosListViewModel(output: PhotosListViewController(),photosRepository: webPhotosRepository)
-        
+        photosListViewModel = PhotosListViewModel(output: PhotosListViewController(), photosRepository: webPhotosRepository)
+
         let promise = XCTestExpectation(description: "No Photos From File Fetched")
         photosListViewModel.search(for: "")
         photosListViewModel.itemsForCollection
@@ -46,13 +45,13 @@ class PhotosListViewModelTests: XCTestCase {
                 XCTAssertEqual(response.count, 0)
                 promise.fulfill()
             }.store(in: &subscriptions)
-        
+
         wait(for: [promise], timeout: 3)
     }
-    
+
     func test_SearchAPISuccess() {
         photosListViewModel = PhotosListViewModel(output: PhotosListViewController())
-        
+
         let promise = XCTestExpectation(description: "Fetching Photos From API Success")
         photosListViewModel.search(for: "photo")
         photosListViewModel.itemsForCollection.dropFirst()
@@ -60,13 +59,13 @@ class PhotosListViewModelTests: XCTestCase {
                 XCTAssertGreaterThan(response.count, 0)
                 promise.fulfill()
             }.store(in: &subscriptions)
-        
+
         wait(for: [promise], timeout: 15)
     }
-    
+
     func test_LoadMoreAPISuccess() {
         photosListViewModel = PhotosListViewModel(output: PhotosListViewController())
-        
+
         let promise = XCTestExpectation(description: "Fetching More Photos From API Success")
         photosListViewModel.search(for: "Tree")
         photosListViewModel.loadMoreData(2)
@@ -75,13 +74,13 @@ class PhotosListViewModelTests: XCTestCase {
                 XCTAssertGreaterThan(response.count, 0)
                 promise.fulfill()
             }.store(in: &subscriptions)
-        
+
         wait(for: [promise], timeout: 15)
     }
-    
+
     func test_GetSearchHistorySuccess() {
         photosListViewModel = PhotosListViewModel(output: PhotosListViewController())
-        
+
         let promise = XCTestExpectation(description: "Fetching Search History Success")
         photosListViewModel.getSearchHistory()
         photosListViewModel.itemsForCollection
@@ -89,7 +88,7 @@ class PhotosListViewModelTests: XCTestCase {
                 XCTAssertGreaterThan(response.count, 0)
                 promise.fulfill()
             }.store(in: &subscriptions)
-        
+
         wait(for: [promise], timeout: 3)
     }
 }
